43da733db934fa767f95103040bb583e8941a531
aymane.lotfi@student-cs.fr <aymane.lotfi@student-cs.fr>
2025-03-19 10:33:13 +0100

    Add sm to smo kernel

--- src/thundersvm/kernel/smo_kernel.cu
+++ src/thundersvm/kernel/smo_kernel.cu
@@ -289,25 +289,50 @@ namespace svm_kernel {
     __global__ void
     update_f_kernel(float_type *f, int ws_size, const float_type *alpha_diff, const kernel_type *k_mat_rows,
                     int n_instances) {
+        extern __shared__ float_type s_alpha_diff[]; // Dynamically allocated shared memory
+        
+        for (int s_idx = threadIdx.x; s_idx < ws_size; s_idx += blockDim.x) {
+            if (s_idx < ws_size) {
+                s_alpha_diff[s_idx] = alpha_diff[s_idx];
+            }
+        }            
         //"n_instances" equals to the number of rows of the whole kernel matrix for both SVC and SVR.
-        KERNEL_LOOP(idx, n_instances) {//one thread to update multiple fvalues.
+       // KERNEL_LOOP(idx, n_instances) {//one thread to update multiple fvalues.
+       int idx = threadIdx.x + blockDim.x * blockIdx.x ;
+        if(idx < n_instances){
             double sum_diff = 0;
             for (int i = 0; i < ws_size; ++i) {
-                double d = alpha_diff[i];
+                float_type d = s_alpha_diff[i];
                 if (d != 0) {
                     sum_diff += d * k_mat_rows[i * n_instances + idx];
                 }
             }
             f[idx] -= sum_diff;
         }
-    }
+}
 
-    void
-    update_f(SyncArray<float_type> &f, const SyncArray<float_type> &alpha_diff, const SyncArray<kernel_type> &k_mat_rows,
-             int n_instances) {
-        SAFE_KERNEL_LAUNCH(update_f_kernel, f.device_data(), alpha_diff.size(), alpha_diff.device_data(),
-                           k_mat_rows.device_data(), n_instances);
+void update_f(SyncArray<float_type> &f, const SyncArray<float_type> &alpha_diff, 
+             const SyncArray<kernel_type> &k_mat_rows, int n_instances) {
+    const int ws_size = alpha_diff.size();
+    const size_t shared_mem = ws_size * sizeof(float_type);
+    //printf("shm :%d \n", shared_mem);
+    const int BLOCK_SIZE = 256;  
+    const int GRID_SIZE = (n_instances + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    
+    update_f_kernel<<<GRID_SIZE, BLOCK_SIZE, shared_mem>>>(
+        f.device_data(), 
+        ws_size,
+        alpha_diff.device_data(),
+        k_mat_rows.device_data(),
+        n_instances
+    );
+    
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Kernel launch error: %s\n", cudaGetErrorString(err));
+        exit(1);
     }
+}
 
     void sort_f(SyncArray<float_type> &f_val2sort, SyncArray<int> &f_idx2sort) {
         thrust::sort_by_key(thrust::cuda::par, f_val2sort.device_data(), f_val2sort.device_data() + f_val2sort.size(),
